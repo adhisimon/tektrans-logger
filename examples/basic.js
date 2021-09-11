90d31e8c2b97185b07b61b2ded35545b5ea1c4bd
Adhidarma Hadiwinoto <adhisimon@gmail.com>
2021-09-11 18:47:10 +0700

    More example on basic

--- examples/basic.js
+++ examples/basic.js
@@ -6,6 +6,7 @@ logger.verbose('This is the most simple log');
 // To set minimum loglevel, you can specify minimum log level by setting this value:
 // - process.env.TEKTRANS_LOGGER_LEVEL
 // - global.TEKTRANS_LOGGER_LEVEL
+logger.silly('This should not be logged because minimum log level is verbose.');
 logger.debug('This should not be logged because minimum log level is verbose.');
 
 logger.verbose('This is an example of log with metadata', {
@@ -15,3 +16,8 @@ logger.verbose('This is an example of log with metadata', {
         c1: 'compund',
     },
 });
+
+logger.http('This in a http log');
+logger.info('This is an info log');
+logger.warn('This is an warn log');
+logger.error('This in an error');
