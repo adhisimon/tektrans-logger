532973f296b76f24dad0a2b81ebf908eccab6544
Adhidarma Hadiwinoto <adhisimon@gmail.com>
2021-09-12 20:38:17 +0700

    Add redis transport

---
+++ examples/redis.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+// publish config object to be used by tektrans-logger
+global.TEKTRANS_LOGGER_CONFIG = {
+    level: 'silly',
+    label: 'REDIS-EXAMPLE',
+
+    // default is "logs" directory on current workdir
+    directory: path.join(process.cwd(), 'logs'),
+
+    filename: 'redis-log',
+
+    // default is using generic level value
+    console_level: null,
+
+    // default is using generic level value
+    file_level: null,
+
+    // default is no old file removal
+    max_files: '10d',
+
+    redis: {
+        host: 'localhost',
+        port: 6379,
+        auth: null,
+        channel: null,
+    },
+};
+
+const logger = require('..');
+
+const sleep = (ms) => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve();
+    }, ms);
+});
+
+const main = async () => {
+    // await sleep(2000);
+
+    logger.silly('This is a silly log.');
+    logger.debug('This is a debug log.');
+    logger.verbose('This is a verbose log');
+
+    // looks like redis transport doesn't support log with metadata,
+    // log with metadata will be skipped by redis transport
+    logger.verbose('This is a verbose example of log with metadata', {
+        a: 'metadata1',
+        b: 'metadata2',
+        c: {
+            c1: 'compund',
+        },
+    });
+
+    logger.http('This in a http log');
+    logger.info('This is an info log');
+    logger.warn('This is an warn log');
+    logger.error('This in an error');
+
+    // give few seconds to make sure all logs
+    // has been emitted to redis before close the log
+    await sleep(2000);
+
+    // don't forget to close the logger at the end
+    logger.end();
+};
+
+main();
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const config = require('./lib/config');
 
 const consoleTransport = require('./lib/transports/console');
 const fileTransport = require('./lib/transports/daily-rotate-file');
+const redisTransport = require('./lib/transports/redis');
 
 const transports = ['Console'];
 
@@ -22,6 +23,14 @@ if (fileTransport) {
     logger.add(fileTransport);
     transports.push('DailyRotateFile');
     logger.debug(`${MODULE_NAME} A2646043: File transport added to logger`);
+}
+
+if (redisTransport) {
+    logger.add(redisTransport);
+    transports.push('Redis');
+    logger.debug(`${MODULE_NAME} 9795F5D2: Redis transport added to logger`, {
+        redisConfig: config.redis,
+    });
 }
 
 logger.verbose(`${MODULE_NAME} AB6F996E: Initialized`, {
